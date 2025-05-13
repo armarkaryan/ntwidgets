d0d355f58893986ef4668e4a82aa2f1c65b6c87f
Arthur Markaryan <armarkaryan.promo@gmail.com>
2025-05-14 01:51:51 +0300

    Added digits_16x16.h

---
+++ src/digits_16x16.h
@@ -0,0 +1,148 @@
+#ifndef DIGITS_16X16_H
+#define DIGITS_16X16_H
+#include <vector>
+#include <string>
+
+// Digit "0"
+const std::vector<std::string> digit_16x16_0 = {
+    "                ",
+    "     XXXXX      ",
+    "    X     X     ",
+    "   X     X X    ",
+    "   X     X X    ",
+    "   X     X  X   ",
+    "   X    X   X   ",
+    "   X    X    X  ",
+    "  X    X     X  ",
+    "  X    X     X  ",
+    "  X    X     X  ",
+    "   X  X      X  ",
+    "    X X     X   ",
+    "     XXXXXXX    ",
+    "                ",
+    "                "
+};
+
+// Digit "1"
+const std::vector<std::string> digit_16x16_1 = {
+    "    X   ",
+    "   XX   ",
+    "  X X   ",
+    "    X   ",
+    "    X   ",
+    "    X   ",
+    "    X   ",
+    "   XXX  "
+};
+
+// Digit "2"
+const std::vector<std::string> digit_16x16_2 = {
+    "  XXXX  ",
+    " X    X ",
+    "      X ",
+    "      X ",
+    "    XX  ",
+    "  XX    ",
+    " X      ",
+    " XXXXXX "
+};
+
+// Digit "3"
+const std::vector<std::string> digit_16x16_3 = {
+    "  XXXX  ",
+    " X    X ",
+    "      X ",
+    "    XX  ",
+    "      X ",
+    "      X ",
+    " X    X ",
+    "  XXXX  "
+};
+
+// Digit "4"
+const std::vector<std::string> digit_16x16_4 = {
+    " X    X ",
+    " X    X ",
+    " X    X ",
+    " XXXXXX ",
+    "      X ",
+    "      X ",
+    "      X ",
+    "      X "
+};
+
+// Digit "5"
+const std::vector<std::string> digit_16x16_5 = {
+    " XXXXXX ",
+    " X      ",
+    " X      ",
+    " XXXXX  ",
+    "      X ",
+    "      X ",
+    " X    X ",
+    "  XXXX  "
+};
+
+// Digit "6"
+const std::vector<std::string> digit_16x16_6 = {
+    "  XXXX  ",
+    " X    X ",
+    " X      ",
+    " XXXXX  ",
+    " X    X ",
+    " X    X ",
+    " X    X ",
+    "  XXXX  "
+};
+
+// Digit "7"
+const std::vector<std::string> digit_16x16_7 = {
+    "  XXXXX ",
+    "      X ",
+    "      X ",
+    "     X  ",
+    "    X   ",
+    "    X   ",
+    "    X   ",
+    "    X   "
+};
+
+// Digit "8"
+const std::vector<std::string> digit_16x16_8 = {
+    "  XXXX  ",
+    " X    X ",
+    " X    X ",
+    "  XXXX  ",
+    " X    X ",
+    " X    X ",
+    " X    X ",
+    "  XXXX  "
+};
+
+// Digit "9"
+const std::vector<std::string> digit_16x16_9 = {
+    "  XXXX  ",
+    " X    X ",
+    " X    X ",
+    " X    X ",
+    "  XXXXX ",
+    "      X ",
+    " X    X ",
+    "  XXXX  "
+};
+
+// Array of digits image from 0 to 9
+const std::vector<std::string> digits_16x16[] = {
+    digit_16x16_0,
+    digit_16x16_1,
+    digit_16x16_2,
+    digit_16x16_3,
+    digit_16x16_4,
+    digit_16x16_5,
+    digit_16x16_6,
+    digit_16x16_7,
+    digit_16x16_8,
+    digit_16x16_9
+};
+
+#endif // DIGITS_16X16_H
